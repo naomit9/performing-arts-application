ed033ba47b233a5e40183a2892cc50a63e138589
meldeleon <mjdeleon08@gmail.com>
2024-03-23 23:01:23 -0400

    Created lesson controller, view models and json file

---
+++ PerformingArtsApplication/Models/ViewModels/UpdateLesson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerformingArtsApplication.Models.ViewModels
+{
+    public class UpdateLesson
+    {
+        public LessonDto SelectedLesson { get; set; }
+    }
+}
