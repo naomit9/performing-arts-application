40abb44c06e1abd37af7d3d157799addce0d8f9e
hannahquintos <hannah@quintos.com>
2024-04-01 23:06:21 -0400

    Built crud functionality for students

---
+++ PerformingArtsApplication/Controllers/StudentController.cs
@@ -0,0 +1,171 @@
+﻿using PerformingArtsApplication.Migrations;
+using PerformingArtsApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Diagnostics;
+
+namespace PerformingArtsApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // code factoring to reduce redundancy
+        // instantiate HttpClient once which can then be re-used through the lifetime of the application
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        static StudentController()
+        {
+            client = new HttpClient();
+            //set base part of path for accessing information in student controller
+            client.BaseAddress = new Uri("https://localhost:44304/api/");
+        }
+
+        // GET: Student/List
+        public ActionResult List(string SearchKey = null)
+        {
+            // get list of students in the system through an HTTP request
+            // GET {resource}/api/studentdata/liststudents
+            // curl https://localhost:44304/api/studentdata/liststudents
+
+            // set the url
+            string url = "studentdata/liststudents/" + SearchKey;
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            IEnumerable<StudentDto> Students = response.Content.ReadAsAsync<IEnumerable<StudentDto>>().Result;
+
+            //Views/Student/List.cshtml
+            return View(Students);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            // communicate with student data api to retrieve one student
+            // curl https://localhost:44304/api/studentdata/findstudent/{id}
+
+            // set the url
+            string url = "studentdata/findstudent/" + id;
+
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            StudentDto SelectedStudent = response.Content.ReadAsAsync<StudentDto>().Result;
+
+            return View(SelectedStudent);
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        // GET: Student/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            string url = "studentdata/addstudent";
+
+            string jsonpayload = jss.Serialize(student);
+
+            /*Debug.WriteLine(jsonpayload);*/
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            // existing student information
+            string url = "studentdata/findstudent/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            StudentDto SelectedStudent = response.Content.ReadAsAsync<StudentDto>().Result;
+
+            return View(SelectedStudent);
+        }
+
+        // POST: Student/Update/5
+        [HttpPost]
+        public ActionResult Update(int id, Student student)
+        {
+            try
+            {
+                //send the request to the API
+                string url = "studentdata/updatestudent/" + id;
+
+                // serialize into JSON
+                string jsonpayload = jss.Serialize(student);
+                /*Debug.WriteLine(jsonpayload);*/
+
+                HttpContent content = new StringContent(jsonpayload);
+                content.Headers.ContentType.MediaType = "application/json";
+
+                // POST: api/StudentData/UpdateStudent/{id}
+                // Header : Content-Type: application/json
+
+                HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                return RedirectToAction("Details/" + id);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/DeleteConfirm/5
+        public ActionResult DeleteConfirm(int id)
+        {
+            // get the student information
+
+            string url = "studentdata/findstudent/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            StudentDto SelectedStudent = response.Content.ReadAsAsync<StudentDto>().Result;
+
+            return View(SelectedStudent);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            //send the request to the API
+            string url = "studentdata/deletestudent/" + id;
+
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+    }
+}
---
+++ PerformingArtsApplication/Controllers/StudentDataController.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PerformingArtsApplication.Models;
+using System.Diagnostics;
+using PerformingArtsApplication.Migrations;
+
+namespace PerformingArtsApplication.Controllers
+{
+    public class StudentDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        /// <summary>
+        ///     Returns a list of all students in the system. If there is a search input, returns a list of students related to the search.
+        /// </summary>
+        /// <param name="SearchKey"> An optional parameter (null if not provided) of the user's search input (as a string) </param>
+        /// <returns>
+        ///     Returns all students in the database including their student id, first name, last name, and birthday
+        /// </returns>
+        /// <example>
+        ///     GET: api/StudentData/ListStudents
+        ///     GET: api/StudentData/ListStudents/Sophia
+        /// </example>
+        [HttpGet]
+        [Route("api/studentdata/liststudents/{SearchKey?}")]
+        public IEnumerable<StudentDto> ListStudents(string SearchKey = null)
+        {
+            //select all from students
+            List<Student> Students = db.Students.ToList();
+
+            //if a searchkey is entered
+            if (!string.IsNullOrEmpty(SearchKey))
+            {
+                //select all students that have first or last names that match the search key
+                Students = db.Students.Where
+                   (s => s.FirstName.Contains(SearchKey) || s.LastName.Contains(SearchKey) || (s.FirstName + " " + s.LastName).Contains(SearchKey)).ToList();
+            }
+
+            List<StudentDto> StudentDtos = new List<StudentDto>();
+
+            Students.ForEach(s => StudentDtos.Add(new StudentDto()
+            {
+                StudentId = s.StudentId,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                Birthday = s.Birthday
+            }
+            ));
+
+            return StudentDtos;
+        }
+
+        /// <summary>
+        ///     Recieves a student id and returns the corresponding student
+        /// </summary>
+        /// <param name="id"> The student's primary key, student id (as an integer) </param>
+        /// <returns>
+        ///     Returns one student for the given id including its student id, first name, last name, and birthday
+        /// </returns>
+        /// <example>
+        ///     GET: api/StudentData/FindStudent/5
+        /// </example>
+        [ResponseType(typeof(Student))]
+        [HttpGet]
+        public IHttpActionResult FindStudent(int id)
+        {
+            Student Student = db.Students.Find(id);
+            StudentDto StudentDto = new StudentDto()
+            {
+                StudentId = Student.StudentId,
+                FirstName = Student.FirstName,
+                LastName = Student.LastName,
+                Birthday = Student.Birthday
+            };
+            if (Student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(StudentDto);
+        }
+
+        /// <summary>
+        ///     Recieves a student id and the updated information about a student, then updates the student's information in the system with the data input
+        /// </summary>
+        /// <param name="id"> The student's primary key, student id (as an integer) of the student to update </param>
+        /// <param name="student"> Updated information about a student (student object as JSON FORM DATA)
+        ///                            - properties of student object include student id, first name, last name, and birthday
+        /// </param>
+        /// <returns>
+        ///     HEADER: 200 (Success, No Content Response)
+        ///         or
+        ///     HEADER: 400 (Bad Request)
+        ///         or
+        ///     HEADER: 404 (Not Found)
+        /// </returns>
+        /// <example>
+        ///   POST: api/StudentData/UpdateStudent/5
+        ///   FORM DATA: Student JSON object
+        /// </example>
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateStudent(int id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //server side validation
+            if (string.IsNullOrEmpty(student.FirstName) || string.IsNullOrEmpty(student.LastName) || student.Birthday == DateTime.MinValue)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != student.StudentId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        /// <summary>
+        ///     Recieves information for a new student, adds the new student and its information to the system
+        /// </summary>
+        /// <param name="student"> New student and its information (student object as JSON FORM DATA)
+        ///                            - properties of student object include student id, first name, last name, and birthday
+        /// </param>        
+        /// <returns>
+        ///     HEADER: 201 (Created)
+        ///     CONTENT: Student Id, Student Data
+        ///         or
+        ///     HEADER: 400 (Bad Request)
+        /// </returns>
+        /// <example>
+        ///     POST: api/StudentData/AddStudent
+        ///     FORM DATA: Student JSON object
+        /// </example>
+        [ResponseType(typeof(Student))]
+        [HttpPost]
+        public IHttpActionResult AddStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            //server side validation
+            if (string.IsNullOrEmpty(student.FirstName) || string.IsNullOrEmpty(student.LastName) || student.Birthday == DateTime.MinValue)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Students.Add(student);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = student.StudentId }, student);
+        }
+
+        /// <summary>
+        ///     Recieves a student id and deletes the corresponding student from the system
+        /// </summary>
+        /// <param name="id"> The student's primary key, student id (as an integer) </param>
+        /// <returns>
+        ///     HEADER: 200 (Ok)
+        ///         or
+        ///     HEADER: 404 (Not Found)
+        /// </returns>
+        /// <example>
+        ///     POST: api/StudentData/DeleteStudent/5
+        ///     FORM DATA: (empty)
+        /// </example>
+        [ResponseType(typeof(Student))]
+        [HttpPost]
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            db.Students.Remove(student);
+            db.SaveChanges();
+
+            return Ok(student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StudentExists(int id)
+        {
+            return db.Students.Count(e => e.StudentId == id) > 0;
+        }
+    }
+}
